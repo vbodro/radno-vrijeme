cca4ec7714ba12d368c1bad1bc2c2f9a8ff2bceb
vbodrozic <vbodrozic@beamng.gmbh>
2023-03-23 16:03:35 +0100

    First submit

---
+++ RadnoVrijeme/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace RadnoVrijeme
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CreateReport = new System.Windows.Forms.Button();
+            this.CurrentDatePicker = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.StartDatePicker = new System.Windows.Forms.DateTimePicker();
+            this.EndDatePicker = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.AddVacationButton = new System.Windows.Forms.Button();
+            this.AddTripButton = new System.Windows.Forms.Button();
+            this.EmployeeComboBox = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.VacationListBox = new System.Windows.Forms.ListBox();
+            this.TripListBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // CreateReport
+            // 
+            this.CreateReport.Location = new System.Drawing.Point(329, 152);
+            this.CreateReport.Name = "CreateReport";
+            this.CreateReport.Size = new System.Drawing.Size(121, 23);
+            this.CreateReport.TabIndex = 0;
+            this.CreateReport.Text = "Create";
+            this.CreateReport.UseVisualStyleBackColor = true;
+            this.CreateReport.Click += new System.EventHandler(this.CreateReport_Click);
+            // 
+            // CurrentDatePicker
+            // 
+            this.CurrentDatePicker.Location = new System.Drawing.Point(28, 30);
+            this.CurrentDatePicker.MaxDate = new System.DateTime(2030, 12, 31, 0, 0, 0, 0);
+            this.CurrentDatePicker.MinDate = new System.DateTime(2018, 1, 1, 0, 0, 0, 0);
+            this.CurrentDatePicker.Name = "CurrentDatePicker";
+            this.CurrentDatePicker.Size = new System.Drawing.Size(157, 20);
+            this.CurrentDatePicker.TabIndex = 1;
+            this.CurrentDatePicker.Value = new System.DateTime(2019, 1, 1, 0, 0, 0, 0);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Date of report";
+            // 
+            // StartDatePicker
+            // 
+            this.StartDatePicker.Location = new System.Drawing.Point(28, 96);
+            this.StartDatePicker.MaxDate = new System.DateTime(2030, 12, 31, 0, 0, 0, 0);
+            this.StartDatePicker.MinDate = new System.DateTime(2018, 1, 1, 0, 0, 0, 0);
+            this.StartDatePicker.Name = "StartDatePicker";
+            this.StartDatePicker.Size = new System.Drawing.Size(129, 20);
+            this.StartDatePicker.TabIndex = 3;
+            this.StartDatePicker.Value = new System.DateTime(2019, 1, 1, 0, 0, 0, 0);
+            this.StartDatePicker.ValueChanged += new System.EventHandler(this.StartDatePicker_ValueChanged);
+            // 
+            // EndDatePicker
+            // 
+            this.EndDatePicker.Location = new System.Drawing.Point(178, 96);
+            this.EndDatePicker.MaxDate = new System.DateTime(2030, 12, 31, 0, 0, 0, 0);
+            this.EndDatePicker.MinDate = new System.DateTime(2018, 1, 1, 0, 0, 0, 0);
+            this.EndDatePicker.Name = "EndDatePicker";
+            this.EndDatePicker.Size = new System.Drawing.Size(129, 20);
+            this.EndDatePicker.TabIndex = 4;
+            this.EndDatePicker.Value = new System.DateTime(2019, 1, 1, 0, 0, 0, 0);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Select Data range";
+            // 
+            // AddVacationButton
+            // 
+            this.AddVacationButton.Location = new System.Drawing.Point(329, 96);
+            this.AddVacationButton.Name = "AddVacationButton";
+            this.AddVacationButton.Size = new System.Drawing.Size(106, 23);
+            this.AddVacationButton.TabIndex = 6;
+            this.AddVacationButton.Text = "Add Vacation";
+            this.AddVacationButton.UseVisualStyleBackColor = true;
+            this.AddVacationButton.Click += new System.EventHandler(this.AddVacationButton_Click);
+            // 
+            // AddTripButton
+            // 
+            this.AddTripButton.Location = new System.Drawing.Point(450, 96);
+            this.AddTripButton.Name = "AddTripButton";
+            this.AddTripButton.Size = new System.Drawing.Size(106, 24);
+            this.AddTripButton.TabIndex = 7;
+            this.AddTripButton.Text = "Add Trip";
+            this.AddTripButton.UseVisualStyleBackColor = true;
+            this.AddTripButton.Click += new System.EventHandler(this.AddTripButton_Click);
+            // 
+            // EmployeeComboBox
+            // 
+            this.EmployeeComboBox.FormattingEnabled = true;
+            this.EmployeeComboBox.Items.AddRange(new object[] {
+            "Vatroslav Bodrožić"});
+            this.EmployeeComboBox.Location = new System.Drawing.Point(208, 30);
+            this.EmployeeComboBox.Name = "EmployeeComboBox";
+            this.EmployeeComboBox.Size = new System.Drawing.Size(348, 21);
+            this.EmployeeComboBox.TabIndex = 8;
+            this.EmployeeComboBox.SelectedIndexChanged += new System.EventHandler(this.EmployeeComboBox_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(214, 14);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Employee";
+            // 
+            // VacationListBox
+            // 
+            this.VacationListBox.FormattingEnabled = true;
+            this.VacationListBox.Location = new System.Drawing.Point(28, 152);
+            this.VacationListBox.Name = "VacationListBox";
+            this.VacationListBox.Size = new System.Drawing.Size(129, 199);
+            this.VacationListBox.TabIndex = 10;
+            // 
+            // TripListBox
+            // 
+            this.TripListBox.FormattingEnabled = true;
+            this.TripListBox.Location = new System.Drawing.Point(178, 152);
+            this.TripListBox.Name = "TripListBox";
+            this.TripListBox.Size = new System.Drawing.Size(129, 199);
+            this.TripListBox.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(585, 370);
+            this.Controls.Add(this.TripListBox);
+            this.Controls.Add(this.VacationListBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.EmployeeComboBox);
+            this.Controls.Add(this.AddTripButton);
+            this.Controls.Add(this.AddVacationButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.EndDatePicker);
+            this.Controls.Add(this.StartDatePicker);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.CurrentDatePicker);
+            this.Controls.Add(this.CreateReport);
+            this.Name = "Form1";
+            this.Text = "Work hours";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CreateReport;
+        private System.Windows.Forms.DateTimePicker CurrentDatePicker;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker StartDatePicker;
+        private System.Windows.Forms.DateTimePicker EndDatePicker;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button AddVacationButton;
+        private System.Windows.Forms.Button AddTripButton;
+        private System.Windows.Forms.ComboBox EmployeeComboBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox VacationListBox;
+        private System.Windows.Forms.ListBox TripListBox;
+    }
+}
+
---
+++ RadnoVrijeme/Form1.cs
@@ -0,0 +1,297 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RadnoVrijeme
+{
+    public partial class Form1 : Form
+    {
+        
+        List<DateTime> holidays = new List<DateTime>();
+        List<DateTime> vacations = new List<DateTime>();
+        List<DateTime> trips = new List<DateTime>();
+
+        int year;
+        int month;
+        DateTime firstDay;
+        DateTime lastDay;
+
+        void InitHolidays()
+        {
+            CultureInfo culture;
+            culture = CultureInfo.CreateSpecificCulture("hr-HR");
+            System.Threading.Thread.CurrentThread.CurrentCulture = culture;
+            
+            holidays.Add(DateTime.Parse("26.12.2022."));
+            holidays.Add(DateTime.Parse("06.01.2023."));
+            holidays.Add(DateTime.Parse("10.04.2023."));
+            holidays.Add(DateTime.Parse("01.05.2023."));
+            holidays.Add(DateTime.Parse("30.05.2023."));
+            holidays.Add(DateTime.Parse("08.06.2023."));
+            holidays.Add(DateTime.Parse("22.06.2023."));
+            holidays.Add(DateTime.Parse("05.08.2023."));
+            holidays.Add(DateTime.Parse("15.08.2023."));
+            holidays.Add(DateTime.Parse("01.11.2023."));
+            holidays.Add(DateTime.Parse("18.11.2023."));
+            holidays.Add(DateTime.Parse("25.12.2023."));
+            holidays.Add(DateTime.Parse("26.12.2023."));
+
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitHolidays();
+            CurrentDatePicker.Value = DateTime.Now.Date;
+            StartDatePicker.Value = DateTime.Now.Date;
+            EndDatePicker.Value = DateTime.Now.Date;
+            EmployeeComboBox.Text = EmployeeComboBox.Items[0].ToString();
+        }
+
+        private bool IsHolliday(DateTime date)
+        {
+            return holidays.Contains(date.Date);
+        }
+
+        private bool IsWeekend(DateTime date)
+        {
+            return date.DayOfWeek == DayOfWeek.Saturday || date.DayOfWeek == DayOfWeek.Sunday;
+        }
+
+        private bool IsVacation(DateTime date)
+        {
+            return vacations.Contains(date.Date);
+        }
+
+        private bool IsTrip(DateTime date)
+        {
+            return trips.Contains(date.Date);
+        }
+
+
+        private String WorkHoursPerDays()
+        {
+            int dayInMounth = 1;
+            int hollidaysCount = 0;
+            int vacationDaysCount = 0;
+            int tripDaysCount = 0;
+            int sumall = 0;
+
+            String output = "";
+            for (DateTime date = firstDay; date.Date < lastDay; date = date.AddDays(1))
+            {
+                output += "<tr>";
+                // day in a month
+                output += "<td style=\"height:3px\">" + dayInMounth + "</td>";
+                // working time
+                if (!IsWeekend(date) && !IsHolliday(date) && !IsVacation(date) || IsTrip(date))
+                {
+                    output += "<td>" + "09:00-17:00" + "</td>";                                        
+                    
+                } else
+                {
+                    output += "<td></td>";                    
+                }
+                // work hours
+                if (!IsWeekend(date) || IsTrip(date))
+                {
+                    output += "<td>8</td>";
+                    sumall++;
+                } else
+                {
+                    output += "<td></td>";
+                }
+
+                // overwork
+                    output += "<td></td>";
+                // sunday
+                output += "<td></td>";
+                // hollidays
+                if (IsHolliday(date) && !IsWeekend(date))
+                {
+                    output += "<td>8</td>";
+                    hollidaysCount++;
+                } else
+                {
+                    output += "<td></td>";
+                }
+                // night work
+                output += "<td></td>";
+                // sick leave
+                output += "<td></td>";
+                // vacation
+                if (IsVacation(date) && !IsWeekend(date))
+                {
+                    output += "<td>8</td>";
+                    vacationDaysCount++;
+                }
+                else
+                {
+                    output += "<td></td>";
+                }
+                // paid leave
+                output += "<td></td>";
+                // non paid leave
+                output += "<td></td>";
+                // trip
+                if (IsTrip(date))
+                {
+                    output += "<td>8</td>";
+                    tripDaysCount++;
+                }
+                else
+                {
+                    output += "<td></td>";
+                }
+                // terrain work
+                output += "<td></td>";
+                // maternity leave
+                output += "<td></td>";
+
+                output += "</tr>";
+                dayInMounth++;
+            }
+
+            // sum of all hours
+            output += "<tr>";
+            output += "<td colspan=\"2\"><b>Ukupno sati:</b></ td>";
+            output += "<td><b>" + sumall*8 + "</b></td>";
+            output += "<td></td>";
+            output += "<td></td>";
+            if (hollidaysCount > 0)
+            {
+                output += "<td><b>" + hollidaysCount * 8 + "</b></td>";
+            } else
+            {
+                output += "<td></td>";
+            }
+            
+            output += "<td></td>";
+            output += "<td></td>";
+            if (vacationDaysCount > 0)
+            {
+                output += "<td><b>" + vacationDaysCount * 8 + "</b></td>";
+            } else
+            {
+                output += "<td></td>";
+            }
+            
+            output += "<td></td>";
+            output += "<td></td>";
+            if (tripDaysCount > 0)
+            {
+                output += "<td><b>" + tripDaysCount * 8 + "</b></td>";
+            } else
+            {
+                output += "<td></td>";
+            }
+            
+            output += "<td></td>";
+            output += "<td></td>";
+
+            output += "</tr>";
+
+            return output;
+        }
+
+        private String SetMonth(String inpLine)
+        {
+            String monthStr = "" + month + "." + year + ".";
+            if (monthStr.Length < 8)
+            {
+                monthStr = "0" + monthStr;
+            }
+            return inpLine.Replace("REPLACEMONTH", monthStr);
+        }
+
+        private String SetEmployee(String inpLine)
+        {
+            String employee = EmployeeComboBox.Text;
+            return inpLine.Replace("REPLACEEMPLOYEE", employee);
+        }
+
+
+        private void CreateReport_Click(object sender, EventArgs e)
+        {
+            // set values depending on a selected date
+            year = CurrentDatePicker.Value.Year;
+            month = CurrentDatePicker.Value.Month;
+            firstDay = new DateTime(year, month, 1);
+            int year2 = month < 12 ? year : year + 1;
+            int month2 = month < 12 ? month + 1 : 1;
+            lastDay = new DateTime(year2, month2, 1);
+
+            // open streams
+            System.IO.StreamReader reader = new System.IO.StreamReader("..\\..\\Template.html");
+            System.IO.StreamWriter writer = new System.IO.StreamWriter("c:\\Temp\\" + month + "." + year + ". - " + EmployeeComboBox.Text +  ".html");
+           
+            // replace input lines with calculated values
+            while (!reader.EndOfStream)
+            {
+                String inpLine = reader.ReadLine();
+                if (inpLine.Contains("REPLACEWORKHOURS"))
+                {
+                    writer.WriteLine(WorkHoursPerDays());
+                }   
+                else if (inpLine.Contains("REPLACEMONTH"))
+                {
+                    writer.WriteLine(SetMonth(inpLine));
+                }
+                else if (inpLine.Contains("REPLACEEMPLOYEE"))
+                {
+                    writer.WriteLine(SetEmployee(inpLine));
+                }
+                else
+                {
+                    writer.WriteLine(inpLine);
+                }
+            }
+
+            // close streams
+            reader.Close();
+            writer.Close();
+        }
+
+        private void StartDatePicker_ValueChanged(object sender, EventArgs e)
+        {
+            EndDatePicker.Value = StartDatePicker.Value;
+        }
+
+        private void AddVacationButton_Click(object sender, EventArgs e)
+        {
+            DateTime firstDay = StartDatePicker.Value.Date;
+            DateTime lastDay = EndDatePicker.Value.Date;
+            for (DateTime date = firstDay; date.Date <= lastDay; date = date.AddDays(1))
+            {
+                if (!IsWeekend(date) && !IsHolliday(date))
+                {
+                    vacations.Add(date);
+                    VacationListBox.Items.Add(date.ToShortDateString());
+                }                
+            }
+        }
+
+        private void AddTripButton_Click(object sender, EventArgs e)
+        {
+            DateTime firstDay = StartDatePicker.Value.Date;
+            DateTime lastDay = EndDatePicker.Value.Date;
+            for (DateTime date = firstDay; date.Date <= lastDay; date = date.AddDays(1))
+            {
+                trips.Add(date);
+                TripListBox.Items.Add(date.ToShortDateString());
+            }
+        }
+
+        private void EmployeeComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
